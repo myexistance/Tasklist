48313dd67bace7b4f41a87d09701bab41e9e02f0
myexistance <joonyoonlives@gmail.com>
2019-07-17 03:34:56 +1200

    filter tasks

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear tasks event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -42,4 +48,35 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Remmove Tasks
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Delete Item?')) {
+    e.target.parentElement.parentElement.remove();
+
+    }
+  }
+}
+
+// Clear Tasks
+function clearTasks(){
+  taskList.innerHTML = '';
+}
+
+
+// Filter Tasks
+function filterTasks(e){
+  const = text =e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach
+  (function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
